4a0bd99784f5140205c989783f27a510cb0b6165
Jarred Black <jarredckblack@gmail.com>
2020-02-07 17:27:08 -0500

    Finished file reading and text input, implemented findBrute().

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,68 @@
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.io.File;
 
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner patternScanner = new Scanner(System.in);
-        String patternString = patternScanner.nextLine();
-
+    public static void main(String[] args) throws IOException {
+        System.out.println("Please enter the name of the file to be loaded: ");
         Scanner dirScanner = new Scanner(System.in);
         String dirString = dirScanner.nextLine();
 
-        String fileName = "C:\\Users\\Jarred\\Documents\\SCHOOL\\SYSC 2100\\Assignment 02";
-
-        List<Character> textFile = Collections.emptyList();
-
-        try {
-            Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
-        } catch(IOException e) {
-            e.printStackTrace();
+        System.out.println("Please enter the the string you wish to search count: ");
+        Scanner patternScanner = new Scanner(System.in);
+        String patternString = patternScanner.nextLine();
+        List<Character> patternChars = new ArrayList<Character>();
+        for (char c : patternString.toCharArray()) {
+            patternChars.add(c);
         }
+
+        String fileName = "D:\\SYSTEM FILES\\DOCS\\COURSES\\SYSC 2100\\SYSC2100Assignment02\\" + dirString;
+
+        List<Character> TextFile = readTextfile(fileName);
+        findBrute(TextFile, patternChars);
     }
 
+    private static List<Character> readTextfile(String fileName) throws IOException{
+        List<Character> returnList = new ArrayList<Character>();
+        Path path = Paths.get(fileName);
+        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)){
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                for (char c : line.toCharArray()) {
+                    returnList.add(c);
+                }
+            }
+        }
+        return returnList;
+    }
 
     public static int findBrute(List<Character> text, List<Character> pattern) {
         int n = text.size();
         int m = pattern.size();
+        int result = 0;
         long startTime = System.currentTimeMillis();
-        for(int i = 0; i <= n - m; i++) { //start at every letter in the text, minus the lenght of our search word.
-            int k = 0; //variable to track indexing through pattern
-            while (k < m && text.get(i + k) == pattern.get(k)) { //while the considered string matches pattern
+        for(int i = 0; i <= n - m; i++) { //start at every letter in the text, minus the length of our search word.
+            int k = 0; //variable to track indexing through pattern.
+            while (k < m && text.get(i + k) == pattern.get(k)) { //while the considered string matches pattern.
                 k++;
             }
             if (k == m) { //if we get all the way through pattern and our considered string still matches.
-                long endTime = System.currentTimeMillis();
-                return i; //substring of text (text[i ... i+m-1]) is a match
+
+                ++result; //substring of text (text[i ... i+m-1]) is a match, so increment the result counter.
             }
         }
-        return -1; //search retuned no matches, return -1.
+        long endTime = System.currentTimeMillis();
+        long TimeTaken = endTime - startTime;
+        System.out.println("Using ArrayLists: " + result + " matches, derived in " + TimeTaken + " milliseconds.");
+        return result;
     }
-
-
-
 }
