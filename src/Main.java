b9f8b2baf7afec9d1980c41211666ff1751aeb09
Jarred Black <JarredCKBlack@gmail.com>
2020-02-10 16:56:15 -0500

    Added Linked list conversion, and incomplete implementation of linked list search function.

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         System.out.println("Please enter the name of the file to be loaded: ");
         Scanner dirScanner = new Scanner(System.in);
@@ -25,10 +24,13 @@ public class Main {
             patternChars.add(c);
         }
 
-        String fileName = "D:\\SYSTEM FILES\\DOCS\\COURSES\\SYSC 2100\\SYSC2100Assignment02\\" + dirString;
+        String fileName = "C:\\Users\\Jarred\\Documents\\SCHOOL\\SYSC 2100\\SYSC2100Assignment02\\" + dirString;
 
         List<Character> TextFile = readTextfile(fileName);
-        findBrute(TextFile, patternChars);
+        findBruteArrayList(TextFile, patternChars);
+        LinkedList<Character> LinkedTextFile = new LinkedList<>(TextFile);
+        LinkedList<Character> LinkedPatternChars = new LinkedList<>(patternChars);
+        findBruteLinkedList(LinkedTextFile, LinkedPatternChars);
     }
 
     private static List<Character> readTextfile(String fileName) throws IOException{
@@ -45,7 +47,28 @@ public class Main {
         return returnList;
     }
 
-    public static int findBrute(List<Character> text, List<Character> pattern) {
+    public static int findBruteArrayList(List<Character> text, List<Character> pattern) {
+        int n = text.size();
+        int m = pattern.size();
+        int result = 0;
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i <= n - m; i++) { //start at every letter in the text, minus the length of our search word.
+            int k = 0; //variable to track indexing through pattern.
+            while (k < m && text.get(i + k) == pattern.get(k)) { //while the considered string matches pattern.
+                k++;
+            }
+            if (k == m) { //if we get all the way through pattern and our considered string still matches.
+
+                ++result; //substring of text (text[i ... i+m-1]) is a match, so increment the result counter.
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        long TimeTaken = endTime - startTime;
+        System.out.println("Using ArrayLists: " + result + " matches, derived in " + TimeTaken + " milliseconds.");
+        return result;
+    }
+
+    public static int findBruteLinkedList(LinkedList<Character> text, LinkedList<Character> pattern) {
         int n = text.size();
         int m = pattern.size();
         int result = 0;
